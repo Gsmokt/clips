35694e80755c4a0e0d028212036098e12ba08b99
Grzegorz Smoktunowicz <biarth@wp.pl>
2022-12-29 20:06:56 +0100

    Initial commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDJSXMSMROMnBIAiR-VNkAtSayLe1dUCKA',
+    authDomain: 'clips-24d02.firebaseapp.com',
+    projectId: 'clips-24d02',
+    storageBucket: 'clips-24d02.appspot.com',
+    messagingSenderId: '926699047072',
+    appId: '1:926699047072:web:cbe24be369207c8b5661c5',
+  },
+};
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  content: ["./src/**/*.{html,ts}"],
+  safelist: ["bg-blue-400", "bg-green-400", "bg-red-400"],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
